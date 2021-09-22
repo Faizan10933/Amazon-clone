ea77646560db396add6dba5d7bfb7f3261ec39f3
Faizan <faizan10933@gmail.com>
2021-09-22 12:25:44 +0530

    Eight Commit

--- mac/settings.py
+++ mac/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 #Managing Media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
