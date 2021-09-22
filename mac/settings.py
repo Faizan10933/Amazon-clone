90559510c6a9cf74ed380c6d133e2ca1fdfb1b02
Faizan <faizan10933@gmail.com>
2021-09-22 10:40:30 +0530

    Second Commit

--- mac/settings.py
+++ mac/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-grq$c7n7c=5)gi^j3cwoc0jn)u7lzfp%n)zjwgg1$o=sz5tdtb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
